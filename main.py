09dcd24ca59d00fd800170a80972b2cf18b1f2b3
Daniel Calvin <daniel.joseph.calvin@gmail.com>
2025-05-12 16:09:01 -0700

    refactored

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
+from stats import get_num_words
+
 def get_book_text(filepath):
   file_contents = ''
   with open(filepath) as f:
     file_contents = f.read()
   return file_contents
 
-def number_of_words(text):
-  return len(text.split())
-
 def main():
   text = get_book_text('./books/frankenstein.txt')
-  print(f"{number_of_words(text)} words found in the document")
+  print(f"{get_num_words(text)} words found in the document")
 
 main()
