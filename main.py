3098eca45fd1104b72d224599c35683eaf51032b
Daniel Calvin <daniel.joseph.calvin@gmail.com>
2025-05-12 16:52:16 -0700

    implement report

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_num_words, get_char_count
+from stats import get_num_words, get_char_count, sort_count
 
 def get_book_text(filepath):
   file_contents = ''
@@ -8,7 +8,15 @@ def get_book_text(filepath):
 
 def main():
   text = get_book_text('./books/frankenstein.txt')
-  print(f"{get_num_words(text)} words found in the document")
-  print(get_char_count(text))
+  count = sort_count(get_char_count(text))
+
+  print("============ BOOKBOT ============")
+  print("Analyzing book found at books/frankenstein.txt...")
+  print("----------- Word Count ----------")
+  print(f"Found {get_num_words(text)} total words")
+  print("--------- Character Count -------")
+  for char_data in count:
+    print(f"{char_data['char']}: {char_data['num']}")
+  print("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -11,3 +11,10 @@ def get_char_count(text):
     else:
       count[char] = 1
   return count
+
+def sort_count(count):
+  result = []
+  for char in count:
+    result.append({"char": char, "num": count[char]})
+  result.sort(key=lambda x: x['num'], reverse=True)
+  return result
